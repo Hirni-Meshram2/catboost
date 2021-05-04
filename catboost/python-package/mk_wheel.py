1efc6a9cbd5e0ed4eb2d70992294790adb696bbf
Hirni-Meshram2 <79629201+Hirni-Meshram2@users.noreply.github.com>
2021-05-04 16:56:46 +0530

    Update mk_wheel.py

--- catboost/python-package/mk_wheel.py
+++ catboost/python-package/mk_wheel.py
@@ -14,7 +14,7 @@ from argparse import ArgumentParser
 
 sys.dont_write_bytecode = True
 
-PL_LINUX = 'manylinux1_x86_64'
+PL_LINUX = 'manylinux2014_aarch64'
 PL_MACOS = 'macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64'
 PL_WIN = 'win_amd64'
 
